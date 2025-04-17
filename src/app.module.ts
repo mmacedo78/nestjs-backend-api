f43e12db0eb7f31c4e6fc0fd12e8daedb4919c5e
Marcelo Macedo <mmacedo78@gmail.com>
2025-04-17 18:01:41 -0300

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from './typeorm/typeorm.module';
 import { UserModule } from './models/user/user.module';
 import { ProductModule } from './models/product/product.module';
 import { CategoryModule } from './models/category/category.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule, UserModule, ProductModule, CategoryModule],
+  imports: [TypeOrmModule, UserModule, ProductModule, CategoryModule, AuthModule],
   controllers: [],
   providers: [],
 })
